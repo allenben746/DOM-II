d870daaa61ad932dbb60d57dae6184c4257b9d10
Ben <allenben@protonmail.com>
2019-06-18 11:21:15 -0500

    Added elements contaning the navigation anchors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const firstChild = document.querySelector("nav a:nth-child(1)");
+firstChild.style.color = "green";
+const secondChild = document.querySelector("nav a:nth-child(2)");
+secondChild.style.color = "green";
+const thirdChild = document.querySelector("nav a:nth-child(3)");
+thirdChild.style.color = "green";
+const forthChild = document.querySelector("nav a:nth-child(4)");
+forthChild.style.color = "green";
